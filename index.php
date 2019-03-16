ce162e5721299112c0cdd57cc797967fc10320fe
Jaimal Dullat <jaimaldullat@gmail.com>
2019-03-16 18:52:27 -0400

    Basic Structure is ready

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Infinite Scrolling - Blog Posts</title>
+
+    <link rel="stylesheet" href="assets/public.css">
+</head>
+<body>
+<section id="blog-posts">
+    <article class="blog-post">
+        <header>
+            <h2>Blog Post - 1</h2>
+            <p>Posted on 12 August, 2019</p>
+        </header>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus magna libero, fermentum sed ultricies
+            vitae, convallis a erat. Donec tincidunt ex sed ex gravida fermentum id vel turpis. Proin nulla nunc,
+            euismod at porttitor a, gravida sit amet enim. Cras volutpat nisi et nulla dignissim, non blandit diam
+            convallis. Nullam in convallis odio. Pellentesque quis rhoncus lorem. Vestibulum facilisis lectus vel massa
+            porta semper. Suspendisse potenti.
+
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sapien ex, euismod eu semper vel, suscipit eu
+            tortor. Quisque lectus odio, suscipit eget porta nec, suscipit lobortis neque. Vivamus eleifend, ex sed
+            molestie convallis, ante lorem molestie enim, quis hendrerit nulla massa eget ante. Phasellus vehicula odio
+            ex, non viverra lacus vehicula ac. Nulla vulputate aliquam quam elementum rhoncus. Aliquam sollicitudin
+            rhoncus ex vitae venenatis. Duis molestie nulla ac gravida tincidunt. Curabitur vitae aliquam mauris. Donec
+            rhoncus dui ut velit fringilla malesuada.</p>
+    </article>
+
+</section>
+<button id="load-more">Load More</button>
+<script src="assets/public.js"></script>
+</body>
+</html>
